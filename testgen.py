350205ec971f7779c723aff381d56af264534f7d
Giang Phạm <pvmgiang21@apcs.fitus.edu.vn>
2025-05-15 21:00:39 +0700

    fix testgen

--- testgen.py
+++ testgen.py
@@ -1,39 +1,84 @@
 # 1. Load Markdown Files
+from ragas.llms import LangchainLLMWrapper
+from ragas.embeddings import LangchainEmbeddingsWrapper
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
 from langchain_community.document_loaders import DirectoryLoader
-from langchain import OpenAI
-from langchain.embeddings.openai import OpenAIEmbeddings
 from dotenv import load_dotenv
 import os
 
 path = "app/api/data/training_data/"
-loader = DirectoryLoader(path, glob="*.md")
-datafile_text = loader.load()
+loader = DirectoryLoader(path, glob="*.md") 
+docs = loader.load()
 
 # 2. Split Text into Chunks (docs)
-from ragas.testset import TestsetGenerator
-
-generator_llm = "gpt-3.5-turbo"
-generator_embeddings = "text-embedding-ada-002"
-
 env = os.getenv("ENV", None)
 
 if not env:
-    if os.path.exist('.env'):
+    if os.path.exists('.env'):
         load_dotenv(dotenv_path='.env')
     else:
         raise Exception(f"Env file file not found")
 
 API_KEY = os.getenv("OPENAI_API_KEY")
 
-embedding = OpenAIEmbeddings(openai_api_key=API_KEY)
+generator_llm = LangchainLLMWrapper(ChatOpenAI(model="gpt-3.5-turbo"))
+generator_embeddings = LangchainEmbeddingsWrapper(OpenAIEmbeddings())
 
-llm = OpenAI(
-    temparature = 0,
-    model_name = generator_llm
+from ragas.testset.transforms.extractors.llm_based import NERExtractor
+from ragas.testset.transforms.splitters import HeadlineSplitter
+
+transforms = [NERExtractor()]
+
+from ragas.testset.persona import Persona
+
+personas = [
+    Persona(
+        name="curious student",
+        role_description="A student who is curious about the world and wants to learn more about different procedures.",
+    ),
+    Persona(
+        name ="citizen",
+        role_description="A citizen who is having trouble with registering procedures on public service portal.",
+    ),
+]
+
+from ragas.testset import TestsetGenerator
+
+generator = TestsetGenerator(llm=generator_llm, embedding_model=generator_embeddings, persona_list=personas)
+
+from ragas.testset.synthesizers.single_hop.specific import (
+    SingleHopSpecificQuerySynthesizer,
 )
 
-generator = TestsetGenerator(llm=llm, embedding_model=generator_embeddings)
-dataset = generator.generate_with_langchain_docs(datafile_text, testset_size=10)
+distribution = [
+    (SingleHopSpecificQuerySynthesizer(llm=generator_llm), 1.0),
+]
+
+import asyncio
+
+async def language_adapt():
+    for query, _ in distribution:
+        prompts = await query.adapt_prompts("vietnamese", llm=generator_llm)
+        query.set_prompts(**prompts)
+
+    dataset = generator.generate_with_langchain_docs(
+        docs[:],
+        testset_size=5,
+        transforms=transforms,
+        query_distribution=distribution,
+    )
+    return dataset
+
+dataset = asyncio.run(language_adapt())
 
 # 3. Save the Testset to a File
 dataset.to_pandas()
+dataset.to_csv("testset.csv")
+
+
+###Requirement
+#pip install ragas
+#pip install langchain-community
+#pip install langchain-openai
+#pip install "unstructured[local-inference]" (if u encounter any error related to unstructured)
